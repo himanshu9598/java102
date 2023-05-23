df93eff48bc9a10df503d1d1cc16d6cd16518dd9
himanshu9598 <himanshupandey84006@gmail.com>
2023-05-23 14:35:47 +0530

    java101 program

---
+++ src/ZipFile.java
@@ -0,0 +1,23 @@
+public class ZipFile {
+
+    public static void main(String[] args) throws IOException {
+        String sourceFile = "test1.txt";
+        FileOutputStream fos = new FileOutputStream("compressed.zip");
+        ZipOutputStream zipOut = new ZipOutputStream(fos);
+
+        File fileToZip = new File(sourceFile);
+        FileInputStream fis = new FileInputStream(fileToZip);
+        ZipEntry zipEntry = new ZipEntry(fileToZip.getName());
+        zipOut.putNextEntry(zipEntry);
+
+        byte[] bytes = new byte[1024];
+        int length;
+        while((length = fis.read(bytes)) >= 0) {
+            zipOut.write(bytes, 0, length);
+        }
+
+        zipOut.close();
+        fis.close();
+        fos.close();
+    }
+}
